c3e448caf53cc108fce9898b339077d343966b59
Nick Jocius <capacity512@yahoo.com>
2022-01-06 10:45:20 -0600

    introduced middleware packages

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+
+require('dotenv').config();
+
+// app
+const app = express();
+
+// import routes
+const authRoutes = require('./routes/auth');
+
+// middleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+// routes middleware
+app.use('/api', authRoutes);
+
+// port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+})
